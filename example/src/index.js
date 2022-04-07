672b6b31b6d269f475674442ddf847695cdc43cf
jakku001 <jakku001@126.com>
2022-04-07 22:37:18 +0800

    update example

--- example/src/index.js
+++ example/src/index.js
@@ -1,22 +1,97 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Modal, { Button } from "../../src";
+import "./index.css";
 
 const App = () => {
   const [modalVisible, updateModalVisible] = useState(false);
+  const [modalVisible2, updateModalVisible2] = useState(false);
+  const [modalVisible3, updateModalVisible3] = useState(false);
+  const [modalVisible4, updateModalVisible4] = useState(false);
   const handleClick = (e) => {
     updateModalVisible(true);
   };
+  const handleClick2 = (e) => {
+    updateModalVisible2(true);
+  };
+
+  const handleClick3 = (e) => {
+    updateModalVisible3(true);
+  };
+
+  const handleClick4 = (e) => {
+    updateModalVisible4(true);
+  };
+
   return (
     <div>
-      <Button onClick={(e) => handleClick(e)}>测试</Button>
+      <Button onClick={(e) => handleClick(e)}>Basic</Button>
+      <Button className="margin-left-10" onClick={(e) => handleClick3(e)}>
+        No-Close
+      </Button>
+      <Button className="margin-left-10" onClick={(e) => handleClick4(e)}>
+        Footer
+      </Button>
+      <Button className={"btn-center"} onClick={(e) => handleClick2(e)}>
+        No-Mask
+      </Button>
       <Modal
         visible={modalVisible}
-        title="测试"
+        title="Title"
         onCancel={(e) => updateModalVisible(false)}
-        maskClosable={false}
       >
-        <div>hello</div>
+        <div>Basic</div>
+      </Modal>
+
+      <Modal
+        visible={modalVisible2}
+        title="Title"
+        onCancel={(e) => updateModalVisible2(false)}
+        mask={false}
+      >
+        <div>mask=&#123;false&#125;</div>
+      </Modal>
+
+      <Modal
+        visible={modalVisible3}
+        title="Title"
+        onCancel={(e) => updateModalVisible3(false)}
+        closable={false}
+      >
+        <div>closable=&#123;false&#125;</div>
+      </Modal>
+
+      <Modal
+        visible={modalVisible4}
+        title="Title"
+        onCancel={(e) => updateModalVisible4(false)}
+        footer={[
+          <Button onClick={(e) => updateModalVisible4(false)}>cancel</Button>,
+        ]}
+      >
+        <div
+          style={{
+            background: "#acacac",
+            padding: "0 10px",
+            width: "fit-content",
+            fontSize: "20px",
+          }}
+        >
+          <span style={{ color: "#ffc600" }}>footer</span>
+          <span style={{ color: "#f59803" }}>=</span>
+          <span style={{ color: "#7abde6" }}>&#123;</span>
+          <span style={{ color: "#fcd401" }}>[</span>
+          <span style={{ color: "#d4e3f4" }}>&lt;</span>
+          <span style={{ color: "#80ffbb" }}>Button</span>
+          <span style={{ color: "#d4e3f4" }}>&gt;</span>
+          <span style={{ color: "#f4f5f6" }}>cancel</span>
+          <span style={{ color: "#d4e3f4" }}>&lt;</span>
+          <span style={{ color: "#d4e3f4" }}>/</span>
+          <span style={{ color: "#80ffbb" }}>Button</span>
+          <span style={{ color: "#d4e3f4" }}>&gt;</span>
+          <span style={{ color: "#fcd401" }}>]</span>
+          <span style={{ color: "#7abde6" }}>&#125;</span>
+        </div>
       </Modal>
     </div>
   );
